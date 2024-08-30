e64b6e6f795fee07d29dbc8943781ede9c2bd3b8
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2024-08-30 17:23:38 -0300

    feat: companies services and controllers

--- src/data/controllers/companies/index.js
+++ src/data/controllers/companies/index.js
@@ -1,4 +1,31 @@
 const services = require("../../services/companies");
 const { Sucessful, Error } = require("../../../class");
 
-module.exports = {};
+module.exports = {
+  all_companies: async (_, resp) => {
+    try {
+      const companies = await services.all_companies();
+      resp.json(new Sucessful(companies));
+    } catch (error) {
+      resp.json(new Error(error));
+    }
+  },
+
+  create_company: async (req, resp) => {
+    try {
+      if (Object.values(req.body).length === 0 || Object.values(req.body).includes(null || undefined || "")) {
+        return resp.json(new Error("Preencha todos os campos!", "Cadastro inválido!"));
+      }
+
+      const userExist = await services.find_company_byusername(req.body.username);
+      if (userExist.length > 0) return resp.json(new Error("Esse usuário já está sendo usado.", "Cadastro inválido!"));
+      const emailExist = await services.find_company_byemail(req.body.email);
+      if (emailExist.length > 0) return resp.json(new Error("Esse e-mail está sendo usado.", "Cadastro inválido!"));
+
+      await services.create_company(req.body);
+      resp.json(new Sucessful(req.body, "Usuário criado com sucesso!"));
+    } catch (error) {
+      resp.json(new Error(error.message));
+    }
+  },
+};
--- src/data/services/companies/index.js
+++ src/data/services/companies/index.js
@@ -1,3 +1,61 @@
 const db = require("../../../database");
 
-module.exports = {};
+module.exports = {
+  find_company_byid: (id) => {
+    return new Promise((accept, reject) => {
+      db.query("SELECT * FROM companies WHERE id = ?", [id], (error, result) => {
+        if (error) return reject(error);
+        accept(result);
+      });
+    });
+  },
+
+  find_company_byemail: (email) => {
+    return new Promise((accept, reject) => {
+      db.query("SELECT * FROM companies WHERE email = ?", [email], (error, result) => {
+        if (error) return reject(error);
+        accept(result);
+      });
+    });
+  },
+
+  find_company_byusername: (username) => {
+    return new Promise((accept, reject) => {
+      db.query("SELECT * FROM companies WHERE username = ?", [username], (error, result) => {
+        if (error) return reject(error);
+        accept(result);
+      });
+    });
+  },
+
+  all_companies: () => {
+    return new Promise((accept, reject) => {
+      db.query("SELECT * FROM companies", (error, result) => {
+        if (error) return reject(error);
+        accept(result);
+      });
+    });
+  },
+
+  create_company: ({ ...data }) => {
+    return new Promise((accept, reject) => {
+      db.query(
+        "INSET INTO companies (username, password, email, name, city, state, country) VALUES (?, ?, ?, ? ,?, ?, ?)",
+        Object.values(data),
+        (error, result) => {
+          if (error) return reject(error);
+          accept(result);
+        }
+      );
+    });
+  },
+
+  delete_company: (id) => {
+    return new Promise((accept, reject) => {
+      db.query("DELETE FROM companies WHERE id = ?", [id], (error, result) => {
+        if (error) return reject(error);
+        accept(result);
+      });
+    });
+  },
+};
